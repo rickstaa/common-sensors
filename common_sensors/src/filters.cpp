a4b37fdeff8dd7cd6e784cc7551acccbc2887623
Joao <joao.pm.santos96@hotmail.com>
2019-03-12 21:36:29 +0000

    it now compiles in melodic

--- common_sensors/src/filters.cpp
+++ common_sensors/src/filters.cpp
@@ -9,6 +9,8 @@
 
 #include "pluginlib/class_list_macros.h"
 
-PLUGINLIB_REGISTER_CLASS(sensor_filter/FootprintFilter, sensor_filter::FootprintFilter, filters::FilterBase<sensor_msgs::LaserScan>)
-PLUGINLIB_REGISTER_CLASS(sensor_filter/NanToInfFilter, sensor_filter::NanToInfFilter, filters::FilterBase<sensor_msgs::LaserScan>)
+//PLUGINLIB_REGISTER_CLASS(sensor_filter/FootprintFilter, sensor_filter::FootprintFilter, filters::FilterBase<sensor_msgs::LaserScan>)
+PLUGINLIB_EXPORT_CLASS(sensor_filter::FootprintFilter, filters::FilterBase<sensor_msgs::LaserScan>)
+//PLUGINLIB_REGISTER_CLASS(sensor_filter/NanToInfFilter, sensor_filter::NanToInfFilter, filters::FilterBase<sensor_msgs::LaserScan>)
+PLUGINLIB_EXPORT_CLASS(sensor_filter::NanToInfFilter, filters::FilterBase<sensor_msgs::LaserScan>)
 //PLUGINLIB_REGISTER_CLASS(sensor_filter/NanRemoveFilter, sensor_filter::NanRemoveFilter, filters::FilterBase<sensor_msgs::LaserScan>)
